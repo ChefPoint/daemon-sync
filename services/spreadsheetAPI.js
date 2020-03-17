a1248ddac136a2697f572d5350e147b0116ae4ea
João <johnyvasconcelos@icloud.com>
2020-03-17 13:35:13 +0000

    .

--- services/spreadsheetAPI.js
+++ services/spreadsheetAPI.js
@@ -4,8 +4,8 @@ const config = require("config");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 exports.addNewRow = async (documentID, sheetID, row) => {
-  console.log("Waiting...");
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  // console.log("Waiting...");
+  // await new Promise(resolve => setTimeout(resolve, 1000));
   const doc = new GoogleSpreadsheet(documentID);
   await doc.useServiceAccountAuth({
     client_email: config.get("auth.google-service-account-email"),
