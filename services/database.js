398caf6dead9cea06bd0a1f89dafdabb4e653374
João de Vasconcelos <johnyvasconcelos@icloud.com>
2021-09-03 16:23:24 +0100

    Removed deprecation warning suppressors on MongoDB connection
    For more info see https://stackoverflow.com/questions/68958221/mongoparseerror-options-usecreateindex-usefindandmodify-are-not-supported

--- services/database.js
+++ services/database.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /* * * * * */
 /* DATABASE */
@@ -6,35 +6,30 @@
 
 /* * */
 /* IMPORTS */
-const config = require("config");
-const mongoose = require("mongoose");
+const config = require('config');
+const mongoose = require('mongoose');
 
 exports.connect = async function () {
-  console.log("Connecting to MongoDB...");
+  console.log('Connecting to MongoDB...');
   await mongoose
-    .connect(config.get("secrets.database-connection-string"), {
-      useFindAndModify: false,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true, // Temporary fixes for deprecation warnings.
-    })
-    .then(() => console.log("Connected."))
+    .connect(config.get('secrets.database-connection-string'))
+    .then(() => console.log('Connected.'))
     .catch((error) => {
-      console.log("Connection to MongoDB failed.");
-      console.log("At database.js > mongoose.connect()");
+      console.log('Connection to MongoDB failed.');
+      console.log('At database.js > mongoose.connect()');
       console.log(error);
       process.exit();
     });
 };
 
 exports.disconnect = async function () {
-  console.log("Closing connection to MongoDB...");
+  console.log('Closing connection to MongoDB...');
   await mongoose
     .disconnect()
-    .then(() => console.log("Disconnected from MongoDB."))
+    .then(() => console.log('Disconnected from MongoDB.'))
     .catch((error) => {
-      console.log("Failed closing connection to MongoDB.");
-      console.log("At database.js > mongoose.disconnect()");
+      console.log('Failed closing connection to MongoDB.');
+      console.log('At database.js > mongoose.disconnect()');
       console.log(error);
     });
 };
